ea5e55ed00e8810636112a9d3bb2626e7c85af32
yasmim freitas <yasmimfreitaz5@gmail.com>
2025-06-25 13:57:24 -0300

    filtro atualizado mas funciona? não sei

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import estilo from '../styles/Filter.module.css'; //importa o css
+import estilo from '../styles/Filter.module.css'; // importa o css
 
 // componente para filtrar por tipo de pokemon
-const Filter = ({ types, selectedType, onSelectType }) => {
+const Filter = ({ types, selectedType, onSelectType, searchTerm, onSearch }) => {
   return (
-    <div>
-      <select className={estilo.filtro} value={selectedType} onChange={(e) => onSelectType(e.target.value)}>
-        <option value="all">Todos</option> {/*valor nulo enquanto não tem uma escolha feita pelo usuario*/}
+    <div className={estilo.filtrosContainer}> {/* container geral */}
+      
+      {/* filtro por tipo */}
+      <select
+        className={estilo.filtro}
+        value={selectedType}
+        onChange={(e) => onSelectType(e.target.value)}
+      >
+        <option value="all">todos</option> {/*valor nulo enquanto não tem uma escolha feita pelo usuario*/}
         {types.map((type) => (
           <option key={type} value={type}>{type}</option>
         ))}
       </select>
+
+      {/* filtro por nome */}
+      <input
+        className={estilo.search}
+        type="text"
+        placeholder="buscar por nome"
+        value={searchTerm}
+        onChange={(e) => onSearch(e.target.value)}
+      />
     </div>
   );
 };
 
 export default Filter;
+
